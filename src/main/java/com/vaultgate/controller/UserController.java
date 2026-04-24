2249767a60db98ba443e0adaf8317afd9c94d0d9
Vaishnavi Kathe <vaishnavikathe0327@gmail.com>
2026-04-24 17:47:40 +0530

    Update the file

---
+++ src/main/java/com/vaultgate/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.vaultgate.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vaultgate.entity.User;
+import com.vaultgate.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
+}
--- src/main/java/com/vaultgate/security/config/SecurityConfig.java
+++ src/main/java/com/vaultgate/security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.vaultgate.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -7,14 +8,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
 		http
-			.csrf(csrf -> csrf.disable())
-			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/api/**").permitAll()
-				.anyRequest().authenticated()
-			);
+	    .csrf(csrf -> csrf.disable())
+	    .authorizeHttpRequests(auth -> auth
+	        .requestMatchers("/api/**").permitAll()
+	        .anyRequest().authenticated()
+	    )
+	    .httpBasic(httpBasic -> httpBasic.disable())
+	    .formLogin(form -> form.disable());
 		
 		return http.build();
 	}
